3f9160ec0ea8995ea410abfc982e428c4174a5ae
AI4Ric <2022254@student.cct.ie>
2023-12-01 11:48:06 +0000

    File upload option added

--- AIDescGen/views.py
+++ AIDescGen/views.py
@@ -1,7 +1,32 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+import os
+from datetime import datetime
+from django.conf import settings
 
 @login_required
-def home(request):
-    return render(request, 'AIDescGen/home.html')
+def file_upload(request):
+    if request.method == 'POST':
+        files = request.FILES.getlist('image_files')
+        if files:
+            # Create a timestamped folder for the upload
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+            user_folder = os.path.join(settings.MEDIA_ROOT, f'user_{request.user.id}', 'images', timestamp)
 
+            # Create the user-specific and timestamped directories if they don't exist
+            os.makedirs(user_folder, exist_ok=True)
+
+            for file in files:
+                fs = FileSystemStorage(location=user_folder)
+                fs.save(file.name, file)
+                # file_url = fs.url(filename) # If you need the URL of saved files
+
+            # Redirect or inform the user of successful upload
+            return HttpResponseRedirect(reverse('home'))
+
+    # Your code to handle GET requests or show the form
+    return render(request, 'AIDescGen/home.html')
--- Image2Descriptions/urls.py
+++ Image2Descriptions/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='AIDescGen/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('AIDescGen.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
