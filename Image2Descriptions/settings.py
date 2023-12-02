487f8907e52365b1ed42ce233d6aa4e2ce85dfc6
AI4Ric <2022254@student.cct.ie>
2023-12-02 10:23:36 +0000

    Project security reinforced by using environmental variables

--- Image2Descriptions/settings.py
+++ Image2Descriptions/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import Config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ojv6%^9r91!%gne@d)cb4gh3%8abc_e*fe75t2colqkg#dq)-a'
+config = Config()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+API_KEY = config('API_KEY')
 
 ALLOWED_HOSTS = []
 
